d8103cbf45efb31b894e0e5519a1a98be236ed4e
Soulwrite <soulwrite@gmail.com>
2017-05-09 17:06:39 +1200

    Update index.php

--- index.php
+++ index.php
@@ -44,18 +44,9 @@ if(!isset( $_SESSION['Account'] )){
 <body>
 	<div>
 		<div class="inputs">
-				<!--<div><a href="loginForm.php">Login </a></div>
-				<div><a href="registrationForm.php">Register </a></div>
-				<div><a href="ticketform.php">Ticket Sale </a></div>-->
 				<div><a class = "button buttonHover" href="unitTesting.php">Unit Testing </a></div>
 				<div><a class = "button buttonHover" href="itemlistingform.php">List an item </a></div>
 			<br>
-			<!--<div><a href="feedbackForm.php">Feedback </a></div>
-			<div><a href="loginprocess.php">Login Process </a></div>
-			<div><a href="paymentfrom.php">paymentform </a></div>
-			<div><a href="registrationForm.php">registration </a></div>
-			<div><a href="registerprocess.php">regoprocess </a></div>
-			<div><a href="ticketform.php">ticketform </a></div>-->
 
 	<?php if( isset($items) ){ ?>
 		<h3> Hot Items </h3>
@@ -94,7 +85,50 @@ if(!isset( $_SESSION['Account'] )){
 				}
 			?>
 		</table>
-	<?php } ?>
+		<br>
+		<form action = "searchItems.php" method = "post">
+			<p>
+				<input class = "button buttonHover" type="submit" value="Search"> 
+				<input type="text/feild" name="query" id="hiddenField" value="'.$row['a_ID'].'">
+			</p>
+		</form>
+		<br>
+		<?php if( isset($searchResult) ){
+			echo '<h3>Results</h3>'.
+			'<table class="listings bottom">'.
+			'<thead>'.
+				'<tr>'.
+					'<th class="name left">Name</th>'.
+					'<th class="desc" >Description</th>'.
+					'<th>Tickets Left</th>'.
+					'<th>Ticket Price</th>'.
+					'<th class="right">Purchase</th>'.
+				'</tr>'.
+			'</thead>';
+			while( $row = $searchResult->fetch_array() ){
+				$rows[] = $row;
+			}
+			foreach( $rows as $row ){
+				echo '<tr>'.
+					'<td class="left right" >'.
+						$row['i_Name'].
+					'</td><td class="right">'.
+						$row['i_Desc'].
+					'</td><td class="right center">'.
+						$row['i_TicketCount'].
+					'</td><td class="right center">$'.
+						$row['i_TicketCost'].
+					'</td><td class="center right">'.
+						'<form action = "ticketForm.php" method = "post">'.
+							'<input type="hidden" name="ownerID" id="hiddenField" value="'.$row['a_ID'].'">'.
+							'<input type="hidden" name="itemName" id="hiddenField" value="'.$row['i_Name'].'">'.
+							'<input class = "button buttonHover" type="submit" value="Buy">'.
+						'</form>'.
+					'</td>'.
+				'</tr>';		
+				}
+
+		 } ?>
 	</div>
 	</div>
 </body>
