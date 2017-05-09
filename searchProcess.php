884c16a66910bc7d24aa6d4a4664addd162d11fe
Bumchafes <Bumchafes@gmail.com>
2017-05-09 16:35:10 +1200

    Update searchProcess.php
    added comments

--- searchProcess.php
+++ searchProcess.php
@@ -3,16 +3,16 @@ session_start();
 
 		include 'dbFunctions.php';
 		$search = ($_POST["searchList"]);
-		$query = "SELECT tag_ID from tags where tag_Title = '$search%';";
-		$searchRow = [];
-		$result = mysqli_query($dbConnect, $query);
-		while($row = mysql_fetch_assoc($result)){
-			$searchRow[] = $row;
-		}
-		for (int i = 0; i < count($searchRow);i++){
-			$query = "SELECT i_Name, a_ID FROM item_tags WHERE tag_ID = ".$searchRow[i].";";
+		$query = "SELECT tag_ID from tags where tag_Title like '$search%';";  // This process searches through the database for all
+		$searchRow = [];	// entries that are like the input searched by the user 
+		$result = mysqli_query($dbConnect, $query); //then searches through the table tags for all tag_ids where the title is similar
+		while($row = mysql_fetch_assoc($result)){ // this then goes through item_tags and finds the item names that have the 
+			$searchRow[] = $row;             // same tag_id -
+		}					// then it finds the entries of that code and prints it to the mainscreen.
+		for (int i = 0; i < count($searchRow);i++){              
+			$query = "SELECT i_Name, a_ID FROM item_tags WHERE tag_ID like ".$searchRow[i].";";
 			echo '<tr>'.
-					'<td class="left right" >'.
+					'<td class="left right" >'. 
 						$searchRow['i_Name'].
 					'</td><td class="right">'.
 						$searchRow['i_Desc'].
