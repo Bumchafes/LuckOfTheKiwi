8c34015cbdad47392a0ebaa88321a992a0db02f3
jay103137 <1031371186@qq.com>
2017-05-19 20:17:32 +1200

    Formula Form Ver.1
    Just the sample form for the caculation formula

---
+++ formulaForm.php
@@ -0,0 +1,16 @@
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+</head>
+<h1>LuckyKiwi</h1>
+<body>
+<div>
+<form action ="formulaProcess.php" method= "post">
+	<p>Enter How much and how many ticket do you want for your item?</p>
+	<p>We will help you caculate the best result.</p>
+	<label>Price:<input type="number" name = "totalPrice"></label>
+	<label>Ticket:<input type="number" name = "totalTicket"></label>
+
+	<p><input type="submit" value="submit"></p>
+</form>
+</div>
+</body>
