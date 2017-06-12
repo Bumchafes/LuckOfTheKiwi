6782327a5b513684c14aba28b2b15c8ec68fe559
kly2themax <bladezchopshop@gmail.com>
2017-06-12 19:49:52 +1200

    Create loginForm.php

--- loginForm.php
+++ loginForm.php
@@ -7,18 +7,21 @@
 	<h1><i>The<span style="color:#00E700">Lucky</span>Kiwi</i></h1>
 <body>
 <br><br><br>
-	<div>
-	<p>username=alpha : password=abc</p>
+	
+	
 		<form class="inputs" action = "loginProcess.php" method = "post">
-			User Name: <input required type="text" name="user_name">
+		<div class="inputs">
+			<p>*username=alpha : password=abc</p>
+			<p>User Name: <input required type="text" name="user_name"><br>
 			Password: <input required type="password" name="pswd">
 			<p>
 				<input class = "button buttonHover" type="submit" value="Login">
 				<input class = "button buttonHover" type="reset" value="Reset">
 				<a class = "button buttonHover" href="registrationForm.php">Register</a>
 			</p>
+			</div>
 		</form>
 		<p><a class = "button buttonHover" href="index.php">Return Home Page </a></p>
-	</div>
+	
 </body>
 </html>
