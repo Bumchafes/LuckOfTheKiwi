04628fda7b0a486720566bc6016069a11bf02d1a
Emanyr11 <emanyr11@gmail.com>
2018-02-22 12:55:05 +1300

    Update G2 File Structure

---
+++ SocketServer/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+ 
+  while(true){
+    sleep(3);
+  }
+
+  return 0;
+}
