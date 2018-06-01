4cb3960592faa6d218a6f230cdac6cf0a991f172
Emanyr11 <emanyr11@gmail.com>
2018-06-01 22:08:01 +1200

    api working with blockchain

--- API/index.js
+++ API/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var app = express();
+var rpc = require('node-json-rpc');
+
+var options = {
+    // int port of rpc server, default 5080 for http or 5433 for https
+    port: 30333,
+    // string domain name or ip of rpc server, default '127.0.0.1'
+    host: '10.100.10.44',
+    // string with default path, default '/'
+    path: '/',
+    // boolean false to turn rpc checks off, default true
+    strict: true
+  };
+   
+// Create a server object with options
+var client = new rpc.Client(options);
 
 
 //localhost:3000/api
-app.get('api', function(res, req){
+app.get('/api', function(res, req){
     res.json({
         text: 'my api!'
     });
@@ -31,7 +46,22 @@ app.get('/api/protected', checkToken, function(req, res){
                 data: data
             });
         }
-    })
+    });
+});
+
+app.get('/api/rpctrigger', checkToken, function(req, res){
+    jwt.verify(req.token, 'my_secret_key', function(err, data){
+            if(err){
+              res.sendStatus(403);
+            }
+            else{
+                makeCall();
+                res.json({
+                text: 'this is rpctrigger',
+                data: data
+            });
+        }
+    });
 });
 
 //checkToken function
@@ -47,6 +77,42 @@ function checkToken(req, res, next){
         res.sendStatus(403);
     }
 }
+
+
+   
+function makeCall(){
+      console.log("inside function");
+      client.call(
+          {
+            "jsonrpc": "2.0",
+            "method": "invoke",
+            "params": ["f06fe9c09df4b80656779a1225ce33acc21add65",[
+                {
+                "type": "String",
+                "value": "buy"
+                },
+                {
+                "type":"String",
+                "value": "ripadamwest"
+                },
+                {
+                "type":"String",
+                "value": "42"
+                }
+                ]
+            ],
+            "id": 1
+        },
+          function (err, res) {
+            // Did it all work ?
+            if (err) { console.log("recieved error " + err); }
+            else { console.log("sever said: " + JSON.stringify(res)); }
+          }
+        );
+        console.log("After call");
+      
+  }
+
 
 app.listen(3000, function(){
     console.log('App listening on port 3000!');
